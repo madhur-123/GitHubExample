fb8ddd35eade74db71e7e914cb8cd20183e0475e
NewMacmini <newmacmini@newmacminis-mac-mini.local>
2018-01-31 13:30:18 +0530

    Initial Commit

--- GitHubExample/ViewController.swift
+++ GitHubExample/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     @IBOutlet weak var randomText: UITextField!
+    
+     
      override func viewDidLoad() {
           super.viewDidLoad()
-          // Do any additional setup after loading the view, typically from a nib.
+
+     
+          let textEntered = randomText.text
+          print("Random Text Entered is :    ", textEntered!)
      }
 
+     
      override func didReceiveMemoryWarning() {
           super.didReceiveMemoryWarning()
           // Dispose of any resources that can be recreated.
